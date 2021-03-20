2cae6e8b4ee5cdafc599512f2f7347b507796960
Kiana Long <kianajade42.kl@gmail.com>
2021-03-20 19:19:54 +0000

    Automatically backed up by Learn

--- db/migrate/20210320190700_recipes.rb
+++ db/migrate/20210320190700_recipes.rb
@@ -2,7 +2,7 @@ class Recipes < ActiveRecord::Migration
   def change
     create_table :recipes do|r|
       r.string :name
-      r.sting  :ingredients
+      r.string  :ingredients
       r.string :cook_time
   end
 end
